f20970df75e578032cb8bf0dc726a3fdde19b5d1
Tony.xu <191284969@qq.com>
2024-09-14 11:52:18 +0800

    modify .gitignore & item_01.py

--- example_code/item_01.py
+++ example_code/item_01.py
@@ -16,6 +16,7 @@
 
 # Reproduce book environment
 import random
+
 random.seed(1234)
 
 import logging
@@ -37,16 +38,18 @@ OLD_CWD = os.getcwd()
 atexit.register(lambda: os.chdir(OLD_CWD))
 os.chdir(TEST_DIR.name)
 
+
 def close_open_files():
     everything = gc.get_objects()
     for obj in everything:
         if isinstance(obj, io.IOBase):
             obj.close()
 
-atexit.register(close_open_files)
 
+atexit.register(close_open_files)
 
 # Example 1
 import sys
+
 print(sys.version_info)
 print(sys.version)
